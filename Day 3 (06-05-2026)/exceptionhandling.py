315991a316487d53b9389d788a98189c43ed64da
CALVINSAJIN <calvinsajin7@gmail.com>
2026-05-13 11:06:04 +0530

    initial commit

--- Day 3 (06-05-2026)/exceptionhandling.py
+++
@@ -1,9 +0,0 @@
-try:    
-    with open('demofile.txt', 'r') as file:
-        content = file.read()
-except FileNotFoundError:    
-    print("The file was not found. Please check the file name and try again.")
-except Exception as e:    
-    print("An error occurred:",e)    
-else:    
-    print("File content:\n",content)
